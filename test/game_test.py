64e81d2f71a9e6476423b8aaac9c705baa215df3
Ahmed Jaffery <ajaffery08@gmail.com>
2021-06-15 20:46:10 -0400

    Class Updates
    Class Updates

--- test/game_test.py
+++ test/game_test.py
@@ -2,3 +2,9 @@
 # TODO: import some code
 
 # TODO: test the code
+
+from app.game import determine_winner
+def test_determination_of_the_winner():
+    assert determine_winner("rock", "rock") == None
+    assert determine_winner("rock", "paper") == "paper"
+    assert determine_winner("rock", "rock") == "rock"
